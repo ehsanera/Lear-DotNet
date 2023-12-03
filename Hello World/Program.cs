3c2b1c69a01c379de69acb738ddebfbff5e5de43
ehsan <ehsan-rafee@hotmail.com>
2023-12-03 13:58:34 +0330

    init

---
+++ Hello World/Program.cs
@@ -0,0 +1,49 @@
+﻿using Hello_World;
+
+var users = new List<User>();
+
+Console.WriteLine("############# Menu #########");
+Console.WriteLine("1- Add");
+Console.WriteLine("2- List");
+Console.WriteLine("3- Update");
+Console.WriteLine("4- Delete");
+Console.WriteLine("0- Exit");
+Console.Write("Please enter your choice: ");
+
+var input = Console.ReadLine();
+
+if (input != null)
+{
+    var selectedMenu = int.Parse(input);
+
+    switch (selectedMenu)
+    {
+        case 1:
+        {
+            Console.Write("Give Me Name: ");
+            var name = Console.ReadLine();
+            if (name != null)
+            {
+                users.Add(new User { Id = users.Count + 1, Name = name });
+            }
+
+            Console.WriteLine(string.Join("count:", users.Count));
+            var list = string.Join(", ", users);
+            Console.WriteLine(list);
+
+            break;
+        }
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 0:
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine("Out Of Range");
+            break;
+    }
+}
---
+++ Hello World/User.cs
@@ -0,0 +1,7 @@
+﻿namespace Hello_World;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
